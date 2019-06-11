cc105e44fb848941d908050a1040f1ebf05a798c
Karol <karol.vogelgezang@gmail.com>
2019-06-11 23:23:04 +0200

    work

--- rncourse/App.js
+++ rncourse/App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -19,8 +19,9 @@ const instructions = Platform.select({
 type Props = {};
 export default class App extends Component<Props> {
   state = {
-    placeName: ''
-  }
+    placeName: '',
+    places: []
+  };
 
   placeNameChangedHandler = val => {
     this.setState({
@@ -28,15 +29,36 @@ export default class App extends Component<Props> {
     });
   };
 
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+        return;
+    }
+
+    this.setState(prevState => {
+        return {
+            places: prevState.places.concat(prevState.placeName)
+        };
+    });
+  };
+
   render() {
+    const placesOutput = this.state.places.map((place, i) => (
+        <Text key={i}>{place}</Text>
+    ));
     return (
       <View style={styles.container}>
-        <TextInput
-            style={{width: 300}}
-            placeholder="Awesome place"
-            value={this.state.placeName}
-            onChange={this.placeNameChangedHandler}
-        />
+        <View style={styles.inputContainer}>
+            <TextInput
+                placeholder="Awesome place"
+                value={this.state.placeName}
+                onChangeText={this.placeNameChangedHandler}
+                style={styles.placeInput}
+            />
+            <Button onPress={this.placeSubmitHandler} title="Add" style={styles.placeButton} />
+        </View>
+        <View>
+            {placesOutput}
+        </View>
       </View>
     );
   }
@@ -44,19 +66,23 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    flex: 1, // dzięki flex:1, nasz contaienr zawiera całą powierzchnie telefonu
+    padding: 20,
+    justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  inputContainer: {
+    // flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  placeInput: {
+    width: "70%"
   },
+  placeButton: {
+    width: "30%"
+  }
 });
